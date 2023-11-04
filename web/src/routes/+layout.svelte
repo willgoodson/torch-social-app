c74229ba3bc6a3f7ec768ce666bc3d40257be9b0
William Goodson <goodsonwill@gmail.com>
2023-11-04 18:21:18 -0500

    Updated frontend

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script>
-    import '../../static/css/main.css'
+    import '$lib/css/main.css'
     import icon from '$lib/assets/torch-icon-color.svg'
 </script>
 <svelte:head>
@@ -23,7 +23,7 @@
     /* NavBar Styling */
     #navbar {
         display: grid;
-        grid-template-columns: 300px auto 120px;
+        grid-template-columns: 300px auto 300px;
         grid-template-rows: 80px;
         grid-template-areas:
         "title . navigation";
@@ -33,6 +33,7 @@
         grid-area: title;
         display: flex;
         align-items: center;
+        margin-left: 20px;
     }
 
     #logo a {
@@ -41,26 +42,31 @@
 
     #logo img{
         width: 30px;
-        margin: 0 10px 0 10px;
+        padding-right: 10px;
     }
 
     #navbar ul {
         grid-area: navigation;
-        display: flex;
         list-style-type: none;
+        padding: 0;
+        text-align: right;
+        display: flex;
+        justify-content: flex-end;
         align-items: center;
-
+        margin-right: 20px;
     }
 
-    #navbar li {
+    #navbar li:not(:last-child) {
         padding-right: 10px;
     }
 
     #navbar li a:hover {
         color: #E9724C;
+      text-align: right;
     }
 
     #navbar a{
+      text-align: right;
         text-decoration: none;
     }
 </style>
